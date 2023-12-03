64a55cac80df26b5926a8cec661cc7a2f743f4b5
h@ckrul3r <hackruler5770@gmail.com>
2023-12-03 15:21:42 +0530

    Update root.sh

--- root.sh
+++ root.sh
@@ -45,35 +45,35 @@ fi
 
 if [ ! -f "$input_file" ]; then
     echo -e "${RED}Error: The specified input file '$input_file' is not present in the directory.${NC}"
-    cexit 1
+    exit 0
 fi
 
 if [ ! -s "$input_file" ]; then
     echo -e "${RED}Error: The specified input file '$input_file' is empty.${NC}"
-    cexit 1
+    exit 0
 fi
 
 if [ ! -r "$input_file" ]; then
     echo -e "${RED}Error: The specified input file '$input_file' is not readable.${NC}"
-    cexit 1
+    exit 0
 fi
 
 echo -e "${YELLOW}[!] Finding root domains from crt.sh${NC}";
 for domain in $(cat "$input_file"); do 
-        curl -s https://crt.sh/?q=${domain}\&output=json --retry 5 | jq | grep -e name_value -e common_name | sort -u | grep \* | cut -d ":" -f2 | sed 's/\"\|\"\,//g' | sed 's/\\n/\n/g' | sed 's/^ *//' | grep \* | sort -u | sed 's/\*\.//g' | tee -a root.txt > /dev/null || cexit $?; 
+        curl -s https://crt.sh/?q=${domain}\&output=json --retry 5 | jq | grep -e name_value -e common_name | sort -u | grep \* | cut -d ":" -f2 | sed 's/\"\|\"\,//g' | sed 's/\\n/\n/g' | sed 's/^ *//' | grep \* | sort -u | sed 's/\*\.//g' | tee -a root.txt > /dev/null; 
         sleep 20
 done
-cat "$input_file" | anew root.txt > /dev/null || cexit $?;
+cat "$input_file" | anew root.txt > /dev/null;
 echo -e "${GREEN}[*] All the root domains are stored to root.txt${NC}";
 echo -e "${GREEN}[**]" $(cat root.txt | wc -l)" root domains found${NC}";
 
 echo -e "${YELLOW}[!] Subfinder is running...${NC}";
-cat root.txt | subfinder -all -silent | sort -u | tee subdomains.txt > /dev/null || cexit $?;
+cat root.txt | subfinder -all -silent | sort -u | tee subdomains.txt > /dev/null;
 echo -e "${GREEN}[*] Subfinder result is stored to subdomains.txt${NC}";
 echo -e "${GREEN}[**]" $(cat subdomains.txt | wc -l)" subdomains found.${NC}";
 
 echo -e "${YELLOW}[!] httpx is running...${NC}";
-httpx -l subdomains.txt -silent -sc -td -cl | tee -a httpx.txt > /dev/null || cexit $?;
+httpx -l subdomains.txt -silent -sc -td -cl | tee -a httpx.txt > /dev/null;
 echo -e "${GREEN}[*] httpx result is stored to httpx.txt${NC}";
 echo "[*] .........................SCRIPT ENDED.....................";
 
