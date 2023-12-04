5827a7724e36a356e653ade5d28eed2f4d2521ab
h@ckrul3r <hackruler5770@gmail.com>
2023-12-05 02:44:02 +0530

    Update root.sh

--- root.sh
+++ root.sh
@@ -1,6 +1,4 @@
 # To find root domains from crt.sh
-# Find subdomains for the respective root domains
-# Also find the httpx result
 
 RED='\033[0;31m'
 GREEN='\033[0;32m'
@@ -15,16 +13,21 @@ cexit() {
 
 show_help() {
     echo "   Usage: bash root.sh -l <root domains file>"
+    echo ""
     echo "   Options:"
-    echo "   -h                          Help Menu"
-    echo "   -l <input_file>             Specify the domain file"
+    echo "   -h                              Help Menu"
+    echo "   -l <input file name>            Specify the domain file."
+    echo "   -o <output file name>           file to write output result."
 }
 
 
 trap 'cexit 1' SIGINT;
 
-while getopts ":l:h" opt; do
+while getopts ":o:l:h" opt; do
     case $opt in
+        o)
+            output_file="$OPTARG"
+            ;;
         l)
             input_file="$OPTARG"
             ;;
@@ -59,20 +62,20 @@ fi
 
 echo -e "${YELLOW}[!] Finding root domains from crt.sh${NC}";
 for domain in $(cat "$input_file"); do 
-        curl -s https://crt.sh/?q=${domain}\&output=json --retry 5 | jq | grep -e name_value -e common_name | sort -u | grep \* | cut -d ":" -f2 | sed 's/\"\|\"\,//g' | sed 's/\\n/\n/g' | sed 's/^ *//' | grep \* | sort -u | sed 's/\*\.//g' | tee -a root.txt > /dev/null; 
+        curl -s https://crt.sh/?q=${domain}\&output=json --retry 5 | jq | grep -e name_value -e common_name | sort -u | grep \* | cut -d ":" -f2 | sed 's/\"\|\"\,//g' | sed 's/\\n/\n/g' | sed 's/^ *//' | grep \* | sort -u | sed 's/\*\.//g' | { [ -z "$output_file" ] && cat || tee -a "$output_file" > /dev/null; }
         sleep 20
 done
-cat "$input_file" | anew root.txt > /dev/null;
-echo -e "${GREEN}[*] All the root domains are stored to root.txt${NC}";
-echo -e "${GREEN}[**]" $(cat root.txt | wc -l)" root domains found${NC}";
+{
+    [ -z "$output_file" ] && 
+    cat ||
+    cat "$input_file" | anew "$output_file" > /dev/null;
+}
 
-echo -e "${YELLOW}[!] Subfinder is running...${NC}";
-cat root.txt | subfinder -all -silent | sort -u | tee subdomains.txt > /dev/null;
-echo -e "${GREEN}[*] Subfinder result is stored to subdomains.txt${NC}";
-echo -e "${GREEN}[**]" $(cat subdomains.txt | wc -l)" subdomains found.${NC}";
 
-echo -e "${YELLOW}[!] httpx is running...${NC}";
-httpx -l subdomains.txt -silent -sc -td -cl | tee -a httpx.txt > /dev/null;
-echo -e "${GREEN}[*] httpx result is stored to httpx.txt${NC}";
-echo "[*] .........................SCRIPT ENDED.....................";
+{
+    [ -z "$output_file" ] && 
+    cat ||
+    echo -e "${GREEN}[**]" $(cat root.txt | wc -l)" root domains found${NC}";
+}
 
+echo -e "${GREEN}[**] ..................SCRIPT ENDED......................${NC}"
